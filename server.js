34ca31ba0e82df44f2231e02b6769504ce065f96
Nripendra0001 <nirpendrapandey13@gmail.com>
2026-01-21 01:04:34 +0530

    add home route

--- server.js
+++ server.js
@@ -21,3 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("🚀 Server running on http://localhost:" + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("SarkariNext Backend is Running 🚀");
+});
+
